0f706fe3ea12eb97583288fec6e5d8e434a4b449
ayush-670 <singhayush160401@gmail.com>
2020-07-25 22:25:05 +0530

    Updates

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 import './getLocation.dart';
 
@@ -11,7 +12,6 @@ class LoadingScreen extends StatefulWidget {
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     getLocation();
   }
@@ -23,8 +23,25 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.long);
   }
 
+  void getData() async {
+    http.Response response = await http.get(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
+    if (response.statusCode == 200) {
+      String data;
+      data = response.body;
+
+       var longitude= jsonDecode(data)['coord']['lon'];
+
+
+      print(longitude);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }
